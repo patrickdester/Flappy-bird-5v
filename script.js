78af455c0ddd82a6f7949e978fb4e5f78647d7d2
SamLouwers <90309503+patrickdester@users.noreply.github.com>
2022-09-07 12:22:33 +0000

    begin

---
+++ script.js
@@ -0,0 +1,52 @@
+var bird;
+
+function setup() {
+  createCanvas(500, 400);
+
+  bird = new Bird(100, 200);
+
+}
+
+
+function draw() {
+  background(225);
+   
+
+
+  bird.drawBird();
+
+}
+
+function keyPressed() {
+  if (keyCode == 32) {
+    bird.vy -= 5;
+  }
+}
+
+class Bird {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vy = 0;
+    this.gravity = 0.2;
+  }
+
+  drawBird() {
+    fill("red");
+    circle(this.x, this.y, 30)
+
+    this.vy += this.gravity;
+
+    this.y += this.vy;
+
+    if (this.y > 380) {
+      this.vy = 0;
+      this.y = 380;
+    }
+
+    if (this.y < 0) {
+      this.vy = 0;
+      this.y = 0;
+    }
+  }
+}
