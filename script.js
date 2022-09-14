2c1247ba3a81671bf789264f664b3e534e555454
SamLouwers <90309503+patrickdester@users.noreply.github.com>
2022-09-14 10:04:17 +0000

    Pipes

--- script.js
+++ script.js
@@ -7,15 +7,31 @@ function setup() {
 
   bird = new Bird(150, 200);
 
+  pipe1 = new Pipes(640, 300, 30, 200, -5, "green")
+  pipe2 = new Pipes(640, 0, 30, 150, -5, "green")
+
+  pipes.push(pipe1)
+  pipes.push(pipe2)
+
+
+  
 }
 
 
 function draw() {
   background(225);
 
+  if (frameCount % 60 == 0) {
+    console.log(framecount);
+    let newPipeBot = new Pipes(640, 300, 30, 200, -5, "green");
+    let newPipeTop = new Pipes(640, 0, 30, 150, -5, "green");
+    
+  }
+
   bird.drawBird();
 
 }
+
 
 function keyPressed() {
   if (keyCode == 32) {
@@ -52,6 +68,17 @@ class Bird {
 }
 
 class Pipes {
-  
-}
+  constructor(x, y, w, h, vx, color)
+   this.x = x;
+   this.y = y;
+   this.w = w;
+   this.h = h;
+   this.vx = vx;
+   this.color = color;
+  }
 
+  drawPipes() {
+    fill(this.color)
+    rect(this.x, this.y, this.w, this.h)
+    this.x = this.x + this.vx;
+  }
