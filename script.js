a94fea6210730e39c1598e2d3b07183c33cf42ec
SamLouwers <90309503+patrickdester@users.noreply.github.com>
2022-09-21 12:12:01 +0000

    collision werkt niet

--- script.js
+++ script.js
@@ -1,63 +1,3 @@
-var bird;
-var pipes = [];
-var spacing;
-var pipe1, pipe2;
-
-function setup() {
-  createCanvas(640, 360);
-
-  bird = new Bird(150, 200);
-
-  pipe1 = new Pipes(640, 300, 30, 200, -5, "green")
-  pipe2 = new Pipes(640, 0, 30, 150, -5, "green")
-
-  pipes.push(pipe1)
-  pipes.push(pipe2)
-
-
-
-}
-
-
-function draw() {
-  background(225);
-
-
-  if (frameCount % 60 == 0) {
-    // console.log(framecount);
-    let newPipeBot = new Pipes(640, 300, 30, 200, -4, "green");
-    let newPipeTop = new Pipes(640, 0, 30, 150, -4, "green");
-    pipes.push(newPipeBot)
-    pipes.push(newPipeTop)
-    console.log(pipes.length)
-  }
-
-  pipes.forEach(pipe => {
-    pipe.drawPipes()
-
-    if (pipes.isColliding()) {
-      this.color = "red";
-    }
-    else {
-      this.color = "green"
-    }
-  });
-
-
-
-  bird.drawBird();
-
-
-
-}
-
-
-function keyPressed() {
-  if (keyCode == 32) {
-    bird.vy -= 5;
-  }
-}
-
 class Bird {
   constructor(x, y) {
     this.x = x;
@@ -96,25 +36,82 @@ class Pipes {
     this.h = h;
     this.vx = vx;
     this.color = color;
-    
   }
 
   drawPipes() {
     fill(this.color)
     this.x = this.x + this.vx;
     rect(this.x, this.y, this.w, this.h)
-  }
+}
+   checkCollision() {
+    if (bird.x + bird.w > this.x && bird.x < this.x + this.w) {
+      this.color ("orange");
 
-
-  isColliding() {
-    //
-    if (bird.x + bird.w.this.x && bird.x < this.x + this.w) {
-
-      if (bird.y + bird.h > this.y && bird.y < this.y + this.h) {
-        return true;
+      //console.log(ypos < this.h, ypos + 50 > this.h + 150)
+      if (bird.y + bird.h > this.y && bird.y > this.y + this.h) {
+         this.color ("red");
+        
       }
     }
-    return false;
+  }
+  //isColliding() {
+
+    //if (bird.x + bird.w > this.x && bird.x < this.x + this.w) {
+     // console.log("x collision")
+      //if (bird.y + bird.h > this.y && bird.y < this.y + this.h) {
+        //this.color = ("red")
+
+      //}
+
+      //console.log("false")
+
+
+}
+
+var bird;
+var pipes = [];
+var spacing;
+var pipe1, pipe2;
+
+function setup() {
+  createCanvas(640, 360);
+
+  bird = new Bird(150, 200);
+
+  pipe1 = new Pipes(640, 300, 30, 200, -5, "green")
+  pipe2 = new Pipes(640, 0, 30, 150, -5, "green")
+
+  pipes.push(pipe1);
+  pipes.push(pipe2);
+}
+
+
+function draw() {
+  background(225);
+
+  if (frameCount % 60 == 0) {
+    // console.log(framecount);
+    let newPipeBot = new Pipes(640, 300, 30, 200, -4, "green");
+    let newPipeTop = new Pipes(640, 0, 30, 150, -4, "green");
+    pipes.push(newPipeBot)
+    pipes.push(newPipeTop)
+    //console.log(pipes.length)
+    
+  }
+
+  pipes.forEach(pipe => {
+    pipe.drawPipes();
+
+  });
+  bird.drawBird();
+}
+
+
+
+function keyPressed() {
+  if (keyCode == 32) {
+    bird.vy -= 5;
   }
 }
+
 
